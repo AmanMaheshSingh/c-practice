1627f8c94695414a7c1560389961703d72c8e252
AmanMaheshSingh <amanmaheshsingh@gmail.com>
2025-11-02 00:03:00 +0530

    Program to calculate the length of the string

---
+++ gfg-interview/GFG_Qs34.c
@@ -0,0 +1,11 @@
+//Program to calculate the length of the string.
+#include <stdio.h>
+
+void main(){
+    char name[] = "Aman Mahesh Singh";
+    int i =0;
+    for(i=0;name[i]!='\0';i++){
+        continue;
+    }
+    printf("Length of String is : %d\n",i);
+}
